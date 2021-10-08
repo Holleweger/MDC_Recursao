5451bf4505ff80af7c1583d9ee4d888471ae08d7
Holleweger <67077734+Holleweger@users.noreply.github.com>
2021-10-07 21:50:45 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -1,12 +1,19 @@
 def calculaMDC(v1,v2):
+  global mdc
   mdc = v1
   if v1 % mdc != 0 or v2 % mdc != 0:
     mdc = calculaMDC(v1 - 1,v2)
   else:
-    print("Seu MDC é : ", mdc)
+    return(0)
     
 
 n = int(input("Digite o primeiro valor : "))
 m = int(input("Digite o segundo valor : "))
+o = int(input("Digite o terceiro valor : "))
+mdc = 0
 
+#Comece sempre pelo menor
 calculaMDC(n,m)
+calculaMDC(mdc,o)
+print("Seu MDC é : ",mdc)
+
